a89378139261b5231483a99577a198c7b113e65a
jogik0067 <jogik0067@gmail.com>
2024-08-13 21:32:12 +0300

    add seventh homework

---
+++ lesson_7v1/url_links.py
@@ -0,0 +1,4 @@
+link_data = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+link_calc = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+link_mag = "https://www.saucedemo.com/"
+#
